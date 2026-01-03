c0a470fdb2a7132716bdaaf55f92acf5b6400bbd
Aimen Zeffanine <abulaithzeffanine@gmail.com>
2026-01-03 10:26:22 +0100

    add midlewares

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 require('dotenv').config();
 const routes = require('./routes');
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
 
 
@@ -8,7 +11,28 @@ const port = process.env.PORT || 5000
 
 const app = express();
 
+app.use(morgan('dev'))
+app.use(cors())
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
 app.use('/', routes)
+
+app.use((req, res, next) => {
+    const err = new Error('Not found')
+    err.status = 404
+    next(err)
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({
+        message: error.message
+    })
+})
+
+
 
 app.listen(port, () => {
     console.log('express is running on port ' + port)
